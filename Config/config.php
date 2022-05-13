1918e0c1763129b3fe3174cefb6bfc8fcee0207a
Butcherman <ronbutcher@att.net>
2022-05-12 19:53:54 -0700

    Initial Commit

---
+++ Config/config.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    /*
+    *   Basic Module Information - Name and Version
+    */
+    'name' => 'AutoAttendantBuilderModule',
+    'ver'  => '0.0.0-dev',
+
+    /*
+    *   Information for the Navbar
+    */
+    'navbar' => [],
+
+    /**
+     * If you would rather have a button link on the Dashboard page, use this section
+     */
+   'dashboard_tool' => [
+       [
+           'name'  => 'Auto Attendant Builder',
+           'icon'  => 'fas fa-headset',
+           'route' => 'AutoAttendantBuilderModule.index',
+       ],
+   ],
+
+    /*
+    *   Information for the Administration home page navigation links
+    */
+    'admin_nav' => [
+        [
+//          'perm_name' => null,                                    //  Name of a specific Permission that will allow this link
+            'name'      => 'AutoAttendantBuilderModule',
+            'icon'      => 'fas fa-tools',
+            'route'     => 'AutoAttendantBuilderModule.index',
+        ],
+    ],
+
+    /*
+    *   Notes if there are migrations that need to be added to the database
+    */
+    'has_migrations' => false,
+
+    /*
+    *   Location for uploaded files - Uncomment if this Module needs its own Disk instance
+    */
+    'disks' => [
+//        'AutoAttendantBuilderModule' => [
+//            'driver' => 'local',
+//            'root'   => storage_path('app/AutoAttendantBuilderModule'),
+//        ],
+    ],
+];
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# AutoAttendantBuilderModule
-A Graphical Tool to help build custom Auto Attendant Menus
+# Tech Bench - Auto Attendant Builder Module
+
+This module is an extension to the Tech Bench application.  It uses a graphical and design by example process to assist users in designing a fully functional Auto Attendant for their business.
