1fbbddc6c66fde17cd57c7bce6b35a5ed1d78833
Butcherman <ronbutcher@att.net>
2022-06-10 17:14:57 -0700

    Rebuilt Index Page - Added Template

---
+++ Resources/js/Pages/Tips.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1 class="text-center">Comming Soon....</h1>
+    </div>
+</template>
+
+<script>
+    import guest from '../../../../../resources/js/Layouts/guest';
+    import Layout from '../Template/Layout.vue';
+
+    export default {
+        layout: [ guest, Layout ],
+        props: {
+            //
+        },
+        data() {
+            return {
+                //
+            }
+        },
+        created() {
+            //
+        },
+        mounted() {
+            //
+        },
+        computed: {
+            //
+        },
+        watch: {
+            //
+        },
+        methods: {
+            //
+        },
+    }
+</script>
